eb51cbc65407b4616800df25aefd5b4707b409e5
Corey Vaillancourt <corey.vaillancourt@workgrid.com>
2021-04-12 14:16:51 -0400

    In addressing #15 I inadvertently forgot to use the `onIonBlur` callback.

--- src/mocks/mockIonCheckbox.tsx
+++ src/mocks/mockIonCheckbox.tsx
@@ -6,11 +6,11 @@ export const mockIonCheckbox: React.FC<{
   onIonFocus: (e: CustomEvent) => void,
   onIonBlur: (e: CustomEvent) => void,
   onClick: (e: React.SyntheticEvent<MouseEvent>) => void;
-}> = ({ checked, onIonChange, onIonFocus, onClick, ...rest }) => {
+}> = ({ checked, onIonChange, onIonFocus, onIonBlur, onClick, ...rest }) => {
 
   return (
     <input type="checkbox" onClick={e => {
       onIonChange(new CustomEvent('ionChange', { detail: { checked: e.currentTarget.checked } }));
-    }} onFocus={() => { onIonFocus(new CustomEvent('ionFocus'))}} onBlur={() => { onIonFocus(new CustomEvent('ionBlur'))}} role="toggle" {...rest} />
+    }} onFocus={() => { onIonFocus(new CustomEvent('ionFocus'))}} onBlur={() => { onIonBlur(new CustomEvent('ionBlur'))}} role="toggle" {...rest} />
   );
 };
