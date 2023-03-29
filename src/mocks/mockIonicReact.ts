195cc7ea4e33e4d902ee35698111acc177da6315
Sean Perkins <sean@ionic.io>
2023-03-28 23:19:25 -0400

    feat: remove component mocks

--- src/mocks/mockIonicReact.ts
+++ src/mocks/mockIonicReact.ts
@@ -1,21 +1,10 @@
 import { mockUseIonPicker } from './hooks/mockUseIonPicker';
-import { mockController } from './mockController';
-import { mockIonCheckbox } from './mockIonCheckbox';
 
 export function mockIonicReact() {
   jest.mock('@ionic/react', () => {
     const rest = jest.requireActual('@ionic/react');
     return {
       ...rest,
-      IonActionSheet: mockController,
-      IonAlert: mockController,
-      IonCheckbox: mockIonCheckbox,
-      IonLoading: mockController,
-      IonPicker: mockController,
-      IonPopover: mockController,
-      IonToast: mockController,
-      IonModal: mockController,
-      IonToggle: mockIonCheckbox,
       useIonPicker: mockUseIonPicker
     };
   });
